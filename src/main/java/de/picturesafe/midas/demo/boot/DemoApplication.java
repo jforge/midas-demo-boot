cb120b09d18761c443444f7f9289dd0b120a848c
Jörg Frommann <jfrommann@picturesafe.de>
2015-05-13 15:19:42 +0200

    Read API key from properties file

--- src/main/java/de/picturesafe/midas/demo/boot/DemoApplication.java
+++ src/main/java/de/picturesafe/midas/demo/boot/DemoApplication.java
@@ -16,11 +16,14 @@
 
 package de.picturesafe.midas.demo.boot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -35,11 +38,15 @@ import javax.servlet.Filter;
 
 @SpringBootApplication
 @Configuration
+@PropertySource("classpath:midas.properties")
 @Import(DemoController.class)
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    @Value("${midas.apikey}")
+    private String apiKey;
 
     @Bean
     public Filter hiddenHttpMethodFilter() {
@@ -48,33 +55,20 @@ public class DemoApplication {
 
     @Bean
     public RestTemplate midasTemplate() {
-        //final HttpHost httpHost = new HttpHost("midas.picturesafe.de", 80, "http");
         final RestTemplate restTemplate = new RestTemplate();
         restTemplate.setInterceptors(Collections.<ClientHttpRequestInterceptor>singletonList(new ClientHttpRequestInterceptor() {
             @Override
             public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
                 final HttpRequest wrapper = new HttpRequestWrapper(request);
-                wrapper.getHeaders().set("apikey", "uRJk9RQXpfa2no5miyJVwDknnduGr9Ey");
+                wrapper.getHeaders().set("apikey", apiKey);
                 return execution.execute(wrapper, body);
             }
         }));
         return restTemplate;
-        /*return new RestTemplate(new HttpComponentsClientHttpRequestFactory() {
-            @Override
-            protected HttpContext createHttpContext(HttpMethod httpMethod, URI uri) {
-                final AuthCache authCache = new BasicAuthCache();
-                final BasicScheme basicScheme = new BasicScheme();
-                authCache.put(httpHost, basicScheme);
+    }
 
-                final HttpClientContext localContext = HttpClientContext.create();
-                localContext.setAuthCache(authCache);
-
-                final BasicCredentialsProvider provider = new BasicCredentialsProvider();
-                provider.setCredentials(new AuthScope(httpHost), new UsernamePasswordCredentials("apikey", "uRJk9RQXpfa2no5miyJVwDknnduGr9Ey"));
-                localContext.setCredentialsProvider(provider);
-
-                return localContext;
-            }
-        });*/
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
