b6d859a4b8bd47ff2c0bb100bf7773d43b0f32ce
Jörg Frommann <jfrommann@picturesafe.de>
2015-05-13 12:05:02 +0200

    Package refactoring

---
+++ src/main/java/de/picturesafe/midas/demo/boot/Text.java
@@ -0,0 +1,13 @@
+package de.picturesafe.midas.demo.boot;
+
+public class Text {
+    String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
